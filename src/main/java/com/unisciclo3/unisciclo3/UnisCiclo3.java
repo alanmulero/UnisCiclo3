24ba18d1863e883167c35436823c36b0dcd62c47
alan.mulero <alan.mulero@gmail.com>
2023-05-24 18:10:59 -0300

    Atividade ciclo 3 Java Swing

---
+++ src/main/java/com/unisciclo3/unisciclo3/UnisCiclo3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.unisciclo3.unisciclo3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alan
+ */
+public class UnisCiclo3 {
+
+    double peso;
+    double altura;
+
+    public static void main(String[] args) {
+
+        String retorno;
+
+    }
+}
